8d95b47a9927ecb9a333b858e32a310bbb1fef42
Andrei-Bogdan Moldovan <91475166+TheJJ144@users.noreply.github.com>
2021-10-02 18:45:34 +0300

    jumptoend.py
    interview question

--- facebook/jumptoend.py
+++ facebook/jumptoend.py
@@ -2,6 +2,7 @@
 """
 facebook interview question->Starting at index 0, for an element n at index i, you are allowed to jump at most n indexes ahead.
 Given a list of numbers, find the minimum number of jumps to reach the end of the list.
+Tested on leetcode
 Input: [3, 2, 5, 1, 1, 9, 3, 4]
 Output: 2
 """
